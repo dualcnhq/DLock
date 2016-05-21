0a1357ff09a1129e75d9fd112a79cd839a8cdd3c
dualcnhq <nhq1829@gmail.com>
2016-05-22 04:49:50 +0800

    Update directory view

--- app/src/main/java/com/dualcnhq/sherlocked/fragments/PostFragment.java
+++ app/src/main/java/com/dualcnhq/sherlocked/fragments/PostFragment.java
@@ -30,7 +30,7 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_primary_contact, container, false);
+        View v = inflater.inflate(R.layout.fragment_post, container, false);
         ButterKnife.bind(this, v);
         return v;
     }
